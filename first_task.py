4eb7802c2c3857817c6af6dfd9d9d00df8d77457
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-17 16:39:49 +0300

    add new function according my task

--- first_task.py
+++ first_task.py
@@ -20,8 +20,9 @@ def perebor(N):
     for i in range(N):
         return i
 
-<<<<<<< HEAD
+for i in range(1,100):
+    if i%2==0 and i%3==0:
+        print(i)
 print("Функционал реализован")
-=======
 print("Thats my result in today")
->>>>>>> 990e86334641584328ad995a6f835a15481b441c
+

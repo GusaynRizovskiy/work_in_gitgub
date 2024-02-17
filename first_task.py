fc70f0bfe478cf5e1cf491e43fb059da83746e08
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-17 17:48:12 +0300

    new features were added

--- first_task.py
+++ first_task.py
@@ -26,6 +26,8 @@ def perebor(N):
 for i in range(1,100):
     if i%2==0 and i%3==0:
         print(i)
-
+print("Функционал реализован")
+print("Thats my result in today")
+print("add new something")
 
 

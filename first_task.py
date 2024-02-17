eefc845e024f8f9bb8c0ffd14f72714ca069658f
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-17 15:46:54 +0300

    the first commit

---
+++ first_task.py
@@ -0,0 +1,14 @@
+"""
+Реализуем парочку простых функций
+"""
+def sum(a,b):
+    return a+b
+def mines(a,b):
+    return a-b
+def mnog(a,b):
+    return a*b
+def dell(a,b):
+    return a/b
+"""
+Реализовали функции сложения, вычитания, умножения, деления
+"""
